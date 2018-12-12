dd1769a873505c42ca2ecfee6ec5599990b84589
Dancsa89 <dancsa89@gmail.com>
2018-12-12 16:30:14 +0100

    comment insert into

--- controllers/flats.js
+++ controllers/flats.js
@@ -1,43 +1,43 @@
-const express = require('express'); 
-const flats = express(); 
-const models = require('../models');
+const express = require('express'); // express változó deklarárálása 
+const flats = express();  // express változó inicializálása flatsként
+const models = require('../models'); // model változo deklarálása
 
 // Index
 
-flats.get('/', (req, res) => {
-  models.Flat.findAll().then(flats => {
-    res.locals.flats = flats;
-    res.json(flats);
+flats.get('/', (req, res) => {  // defalut rootra küldött GET request esetén
+  models.Flat.findAll().then(flats => { // a Flat modell alapján megkeresi az összes "lakást"
+    res.locals.flats = flats; // betölti a localsba
+    res.json(flats); // json objektumként visszaküldi
   });
 });
 
 // Show
 
-flats.get('/:id', (req, res) => {
-  models.Flat.findById(req.params.id).then(flat => {
-    res.locals.flats = flats;
-    res.json(flat);
+flats.get('/:id', (req, res) => { // paraméterezett GET requestet (id) esetén
+  models.Flat.findById(req.params.id).then(flat => { // a Flat modell alapján megkeresi az adott id-jű lakást
+    res.locals.flats = flats; // betölti a localsba
+    res.json(flat); // json objektumként visszaküldi azt az egy lakást
   });
 });
 
 // Create
 
-flats.post('/', (req, res) => {
-  models.Flat.create({
-    title: req.body.title,
-    price: req.body.price,
-    floorArea: req.body.floorarea,
-    country: req.body.country,
-    zip: req.body.zip,
-    city: req.body.city,
-    street: req.body.street
-  }).then(flat => {
-    res.locals.flats = flats;
-    res.json(flat);
+flats.post('/', (req, res) => { // defalut rootra küldött POST request esetén
+  models.Flat.create({ // a Flat modell alapján készít egy új lakást, ahol
+    title: req.body.title, // request paraméterként a title
+    price: req.body.price, // price
+    floorArea: req.body.floorarea, // floorarea
+    country: req.body.country, //country
+    zip: req.body.zip, // zip
+    city: req.body.city, // city
+    street: req.body.street //street megadásra kerül
+  }).then(flat => { 
+    res.locals.flats = flats; // amelyet betölt a localstba
+    res.json(flat); // json objektumként visszadja
   });
 });
 
-// Update
+// Update    NEM MŰKÖDIK
 
 flats.put('/:id', (req, res) => {
   models.Flat.findOne( {where: {id: req.params.id}).then(flat => {
@@ -58,9 +58,9 @@ flats.put('/:id', (req, res) => {
 
 // Delete
 
-flats.delete('/:id', (req, res) => {
-  models.Flat.destroy({ where: {id: req.params.id} }).then(result => {
-    res.json(result);
+flats.delete('/:id', (req, res) => { // paraméterezett DELETE requestet (id) esetén
+  models.Flat.destroy({ where: {id: req.params.id} }).then(result => { // // a Flat modell alapján megkeresi az adott id-jű lakást és törli
+    res.json(result); // eredmény jsonnel tér vissza
   });
 });
 module.exports = flats;
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const flats = require('./controllers/flats');
+const express = require('express'); // deklarálok egy express változót, amely az express module-t használja
+const app = express(); // deklarálok egy app változót, amely az express module, express metódusát használja
+const bodyParser = require('body-parser'); // deklarálok egy bodyparser változót, amely a body-parser module-t használja
+const flats = require('./controllers/flats'); // deklarálok egy flats változót, amely a contollers mappában lévő flats.js fájl exportált elemeit tartalmazza
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/flats', flats);
+app.use(bodyParser.json()); // middlevarek inicializálása 
+app.use(bodyParser.urlencoded({ extended: false })); // szintén
+app.use('/flats', flats); // szintém
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {  // futtatáskor a terminálban a 4200 porton figyel az alkalmazás 
   console.log('Listen on 4200');
 });
